49add9f8094b9fc9cc9cb38393ea81d1bcd9d0d0
Besal Raj <besalraj@Besals-MacBook-Pro.local>
2024-09-09 01:31:36 +0530

    Feat: Add new note

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,30 @@ function App() {
       id: 1,
       text: "Note 2"
     }
-  ])
+  ]);
+
+  const [text, setText] = useState('');
 
   return (
     <>
-      <input placeholder='Input text content for new note' /> 
-      <button>Add Note</button>
+      <div style={{
+        backgroundColor: "revert",
+        border: "none",
+        width: "200px",
+        height: "200px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "auto",
+        position: "relative"
+      }}>
+        <input placeholder='Input text content for new note' value={text} onChange={(e) => setText(e.target.value)}/> 
+        <button onClick={() => {setNotes([...notes,
+          {id: notes.length, text: text}
+        ]
+          )
+          setText('')}}>Add Note</button>
+      </div>
 
       <Notes notes={notes} setNotes={setNotes} />
     </>
